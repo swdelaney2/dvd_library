763287fa29839e977bf8d89b87c81db94fd1f074
Stephen Delaney <swdelaney2@gmail.com>
2016-03-11 16:31:07 -0600

    starting to add ajax functionality with omdb

---
+++ app/assets/javascripts/ajax.js
@@ -0,0 +1,21 @@
+function loadMovieInfo(searchTerm) {
+  var ajaxArgument = {
+    type: 'get',
+    url: 'http://www.omdbapi.com/?i=' + searchTerm + '&y=&plot=short&r=json',
+    dataType: 'json',
+    success: function(data) {
+        console.log("success");
+        console.log(data);
+        $('#movieInfo').append("<a href='http://www.imdb.com/title/" + searchTerm + "' target=_blank>" + data.Title + "</a> was released in " + data.Year + ".");
+
+    },
+    error: function(error) {
+        console.log("error")
+        console.log(error);
+    }
+};
+// make the ajax call
+$.ajax(ajaxArgument);
+}
+
+loadMovieInfo('tt0275022');
