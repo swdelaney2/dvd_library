936cafe00b5ab99386f58b8d0280d3703f99bf78
Stephen Delaney <swdelaney2@gmail.com>
2016-03-13 20:37:26 -0500

    search function working, including fallback photo when none is available

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -1,3 +1,19 @@
+$(document).on('page:load', function() {
+$('#search_form').submit(function(e) {
+    e.preventDefault();
+    var keyword = $(":input[name=keyword]", this).val();
+    searchMovieByTitle(keyword)
+});
+});
+
+$(document).ready(function() {
+$('#search_form').submit(function(e) {
+    e.preventDefault();
+    var keyword = $(":input[name=keyword]", this).val();
+    searchMovieByTitle(keyword)
+});
+});
+
 function loadMovieInfo(searchTerm) {
   var ajaxArgument = {
     type: 'get',
@@ -7,6 +23,34 @@ function loadMovieInfo(searchTerm) {
         console.log("success");
         console.log(data);
         $('#movieInfo').append("<p><a href='http://www.imdb.com/title/" + searchTerm + "' target=_blank>" + data.Title + "</a> was released in " + data.Year + ".");
+
+    },
+    error: function(error) {
+        console.log("error")
+        console.log(error);
+    }
+};
+// make the ajax call
+$.ajax(ajaxArgument);
+}
+
+function searchMovieByTitle(searchTerm) {
+  var ajaxArgument = {
+    type: 'get',
+    url: 'http://www.omdbapi.com/?s=' + searchTerm + '&y=&plot=short&r=json',
+    dataType: 'json',
+    success: function(data) {
+        console.log("success");
+          $('#searchResults').html('');
+        // console.log(data.Search[0]);
+        // console.log(data.Search.length);
+        for (var i = 0; i < data.Search.length; i++) {
+          $('#searchResults').append('<ul><li><img src="' + data.Search[i].Poster + '" onError="this.onerror=null;this.src=\'/images/poster-not-available.jpg\';"></li><ul><li><strong>' + data.Search[i].Title + '</strong> (' + data.Search[i].Year + ')</li></ul></ul>');
+        }
+
+        // $('#searchResults').append('test');
+
+
 
     },
     error: function(error) {
