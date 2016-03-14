cf7a1b6ebe575b2b97ad631cd5d4aa505200b813
Stephen Delaney <swdelaney2@gmail.com>
2016-03-13 21:04:39 -0500

    troubleshooting issue with authenticity token

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -42,14 +42,14 @@ function searchMovieByTitle(searchTerm) {
     success: function(data) {
         console.log("success");
           $('#searchResults').html('');
+
         // console.log(data.Search[0]);
         // console.log(data.Search.length);
         for (var i = 0; i < data.Search.length; i++) {
           $('#searchResults').append('<ul><li><img src="' + data.Search[i].Poster + '" onError="this.onerror=null;this.src=\'/images/poster-not-available.jpg\';"></li><ul><li><strong>' + data.Search[i].Title + '</strong> (' + data.Search[i].Year + ')</li></ul></ul>');
         }
 
-        // $('#searchResults').append('test');
-
+// '<form action="/movies" method="post"><input type="hidden" name="authenticity_token" value="<%= form_authenticity_token %>"><input type="hidden" name="movie[imdbID]" id="movie[imdbID]" value="' + data.Search[i].imdbID + '"><input type="submit" name="commit" value="Add Movie"/></form>'
 
 
     },
